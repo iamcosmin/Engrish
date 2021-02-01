432c29c563b40c421def284d549660a38d40215d
iamcosmin <47219753+iamcosmin@users.noreply.github.com>
2021-02-01 15:58:55 +0200

    Prepare for null safety

--- lib/Core/Method/Database/Database.dart
+++ lib/Core/Method/Database/Database.dart
@@ -12,7 +12,7 @@ class Database extends Core {
   //* Exception methods
 
   /// Critical exception if somehow the user bypasses the OOBE and the info is not cached
-  Future exceptOnNonCached({@required List<String> exceptions}) async {
+  Future exceptOnNonCached({/*required*/ @required List<String> exceptions}) async {
     var pfs = await SharedPreferences.getInstance();
     for (var exception in exceptions) {
       await pfs.setBool(exception, false);
